99ec1df13aeb7bf12ef95935070cdf766ae50ab4
MohammedKhalidEk <mohamednaser.mn91@gmail.com>
2018-10-23 12:41:26 +0200

    Add GET /users/me and authenticate middleware

---
+++ server/middleware/authenticate.js
@@ -0,0 +1,21 @@
+var {User} = require('./../models/user');
+
+// middleware to make the route private
+var authenticate = (req, res, next) => {
+    var token = req.header('x-auth');
+
+    User.findByToken(token).then((user) => {
+        if (!user) {
+            // res.status(401).send(); OR
+            return Promise.reject();
+        }
+
+        req.user = user;
+        req.token = token;
+        next();
+    }).catch((e) => {
+        res.status(401).send();
+    });
+}; 
+
+module.exports = {authenticate};
--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 // var newTodo = new Todo({
 //     text: 'Walk the dog'
@@ -119,10 +120,14 @@ app.post('/users', (req, res) => {
         return user.generateAuthToken();
         // res.send(user);
     }).then((token) => {
-        res.header('X-auth', token).send(user);
+        res.header('x-auth', token).send(user);
     }).catch((e) => {
         res.status(400).send(e);
     });
+});
+
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user);
 });
 
 // get user
